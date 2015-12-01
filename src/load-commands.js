8fce495e4d59d269bd3ed8fe6350b28fae005c08
Luigi Poole <luigipoole@outlook.com>
2015-11-30 16:30:48 -0800

    always use node

--- src/load-commands.js
+++ src/load-commands.js
@@ -1,33 +1,18 @@
 'use strict'
 
-const isNode = !global.window
-
-function requireDir () {
-  if (isNode) return require('require-dir')('./api')
-
-  // Webpack specific require of a directory
-  const req = require.context('./api', false, /\.js$/)
-
-  const files = {}
-  req.keys().forEach(key => {
-    const name = key
-      .replace(/^\.\//, '')
-      .replace(/\.js$/, '')
-    files[name] = req(key)
-  })
-
-  return files
+function requireDir() {
+    return require('require-dir')('./api');
 }
 
-function loadCommands (send) {
-  const files = requireDir()
-  const cmds = {}
+function loadCommands(send) {
+    const files = requireDir()
+    const cmds = {}
 
-  Object.keys(files).forEach(file => {
-    cmds[file] = files[file](send)
-  })
+    Object.keys(files).forEach(file => {
+        cmds[file] = files[file](send)
+    })
 
-  return cmds
+    return cmds
 }
 
 module.exports = loadCommands
