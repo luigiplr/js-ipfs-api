2e36b371a7f28613f5062d39ba8b455549569962
Luigi Poole <luigipoole@outlook.com>
2015-12-01 18:40:23 -0800

    kill that

--- src/request-api.js
+++ src/request-api.js
@@ -8,114 +8,115 @@ const isNode = !global.window
 
 // -- Internal
 
-function parseChunkedJson (res, cb) {
-  const parsed = []
-  res.on('data', chunk => {
-    try {
-      parsed.push(JSON.parse(chunk))
-    } catch (err) {
-      // Browser quirks emit more than needed sometimes
-    }
-  })
-  res.on('end', () => cb(null, parsed))
+function parseChunkedJson(res, cb) {
+    const parsed = []
+    res.on('data', chunk => {
+        try {
+            parsed.push(JSON.parse(chunk))
+        } catch (err) {
+            // Browser quirks emit more than needed sometimes
+        }
+    })
+    res.on('end', () => cb(null, parsed))
 }
 
-function onRes (buffer, cb) {
-  return (err, res) => {
-    if (err) {
-      return cb(err)
-    }
-
-    const stream = !!res.headers['x-stream-output']
-    const chunkedObjects = !!res.headers['x-chunked-output']
-    const isJson = res.headers['content-type'] === 'application/json'
-
-    if (res.statusCode >= 400 || !res.statusCode) {
-      const error = new Error(`Server responded with ${res.statusCode}`)
-
-      Wreck.read(res, {json: true}, (err, payload) => {
+function onRes(buffer, cb) {
+    return (err, res) => {
         if (err) {
-          return cb(err)
+            return cb(err)
         }
-        if (payload) {
-          error.code = payload.Code
-          error.message = payload.Message
+
+        const stream = !!res.headers['x-stream-output']
+        const chunkedObjects = !!res.headers['x-chunked-output']
+        const isJson = res.headers['content-type'] === 'application/json'
+
+        if (res.statusCode >= 400 || !res.statusCode) {
+            const error = new Error(`Server responded with ${res.statusCode}`)
+
+            Wreck.read(res, {
+                json: true
+            }, (err, payload) => {
+                if (err) {
+                    return cb(err)
+                }
+                if (payload) {
+                    error.code = payload.Code
+                    error.message = payload.Message
+                }
+                cb(error)
+            })
         }
-        cb(error)
-      })
+
+        // console.log('stream:', stream, ' chunked:', chunkedObjects)
+
+        if (stream && !buffer) return cb(null, res)
+
+        if (chunkedObjects) {
+            if (isJson) return parseChunkedJson(res, cb)
+
+            return Wreck.read(res, null, cb)
+        }
+
+        Wreck.read(res, {
+            json: isJson
+        }, cb)
     }
-
-    // console.log('stream:', stream, ' chunked:', chunkedObjects)
-
-    if (stream && !buffer) return cb(null, res)
-
-    if (chunkedObjects) {
-      if (isJson) return parseChunkedJson(res, cb)
-
-      return Wreck.read(res, null, cb)
-    }
-
-    Wreck.read(res, {json: isJson}, cb)
-  }
 }
 
-function requestAPI (config, path, args, qs, files, buffer, cb) {
-  qs = qs || {}
-  if (Array.isArray(path)) path = path.join('/')
-  if (args && !Array.isArray(args)) args = [args]
-  if (args) qs.arg = args
-  if (files && !Array.isArray(files)) files = [files]
+function requestAPI(config, path, args, qs, files, buffer, cb) {
+    qs = qs || {}
+    if (Array.isArray(path)) path = path.join('/')
+    if (args && !Array.isArray(args)) args = [args]
+    if (args) qs.arg = args
+    if (files && !Array.isArray(files)) files = [files]
 
-  if (typeof buffer === 'function') {
-    cb = buffer
-    buffer = false
-  }
-
-  if (qs.r) {
-    qs.recursive = qs.r
-    delete qs.r // From IPFS 0.4.0, it throw an error when both r and recursive are passed
-  }
-
-  if (!isNode && qs.recursive && path === 'add') {
-    return cb(new Error('Recursive uploads are not supported in the browser'))
-  }
-
-  qs['stream-channels'] = true
-
-  let stream
-  if (files) {
-    stream = getFilesStream(files, qs)
-  }
-
-  // this option is only used internally, not passed to daemon
-  delete qs.followSymlinks
-
-  const opts = {
-    method: files ? 'POST' : 'GET',
-    uri: `${config.protocol}://${config.host}:${config.port}${config['api-path']}${path}?${Qs.stringify(qs, {arrayFormat: 'repeat'})}`,
-    headers: {}
-  }
-
-  if (isNode) {
-    // Browsers do not allow you to modify the user agent
-    opts.headers['User-Agent'] = config['user-agent']
-  }
-
-  if (files) {
-    if (!stream.boundary) {
-      return cb(new Error('No boundary in multipart stream'))
+    if (typeof buffer === 'function') {
+        cb = buffer
+        buffer = false
     }
 
-    opts.headers['Content-Type'] = `multipart/form-data; boundary=${stream.boundary}`
-    opts.downstreamRes = stream
-    opts.payload = stream
-  }
+    if (qs.r) {
+        qs.recursive = qs.r
+        delete qs.r // From IPFS 0.4.0, it throw an error when both r and recursive are passed
+    }
 
-  Wreck.request(opts.method, opts.uri, opts, onRes(buffer, cb))
+
+    qs['stream-channels'] = true
+
+    let stream
+    if (files) {
+        stream = getFilesStream(files, qs)
+    }
+
+    // this option is only used internally, not passed to daemon
+    delete qs.followSymlinks
+
+    const opts = {
+        method: files ? 'POST' : 'GET',
+        uri: `${config.protocol}://${config.host}:${config.port}${config['api-path']}${path}?${Qs.stringify(qs, {arrayFormat: 'repeat'})}`,
+        headers: {}
+    }
+
+    if (isNode) {
+        // Browsers do not allow you to modify the user agent
+        opts.headers['User-Agent'] = config['user-agent']
+    }
+
+    if (files) {
+        if (!stream.boundary) {
+            return cb(new Error('No boundary in multipart stream'))
+        }
+
+        opts.headers['Content-Type'] = `multipart/form-data; boundary=${stream.boundary}`
+        opts.downstreamRes = stream
+        opts.payload = stream
+    }
+
+    Wreck.request(opts.method, opts.uri, opts, onRes(buffer, cb))
 }
 
 // -- Interface
 
-exports = module.exports = function getRequestAPI (config) {
-  return requestAPI.bind(null, config)
+exports = module.exports = function getRequestAPI(config) {
+    return requestAPI.bind(null, config)
 }
